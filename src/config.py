96e89c950e6b161429a41e9827bdc30aa266c300
henan.wang <henan.wang@henan.wang's MacBook Pro>
2018-09-04 10:10:54 +0800

    reformat code

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*- 
-#Author: Xusong Chen
+# Author: Xusong Chen
 
 from __future__ import absolute_import
 from __future__ import division
@@ -8,33 +8,26 @@ from __future__ import print_function
 
 
 class Config:
-  PHASE = 'test'
+    PHASE = 'test'
 
-  BATCH_SIZE = 128
-  MAX_EPOCH = 20
-  INITIAL_LEARNING_RATE = 0.001
-  REG = 0.00005
-  DROPOUT = 0.8
+    BATCH_SIZE = 128
+    MAX_EPOCH = 20
+    INITIAL_LEARNING_RATE = 0.001
+    REG = 0.00005
+    DROPOUT = 0.8
 
-  DATA_DIR = '../data/input'
-  OUT_DIR = '../data/output'
-  RESTORE_MODEL_DIR = '../data/input'
-  OPTIMIZER = 'adam'
+    DATA_DIR = '../data/input'
+    OUT_DIR = '../data/output'
+    RESTORE_MODEL_DIR = '../data/input'
+    OPTIMIZER = 'adam'
 
-  NEG_RATIO = 1.0
-  FUSION_LAYERS = [128]
+    NEG_RATIO = 1.0
+    FUSION_LAYERS = [128]
 
-  MAX_LENGTH = 160
-  ITEM_DIM = 64
-  CATE_DIM = 64
-  USER_DIM = 128
-  DISPLAY = 10
-  NUM_HEADS = 8
-  N_BLOCK = 8
-
-
-
-
-
-
-
+    MAX_LENGTH = 160
+    ITEM_DIM = 64
+    CATE_DIM = 64
+    USER_DIM = 128
+    DISPLAY = 10
+    NUM_HEADS = 8
+    N_BLOCK = 8
